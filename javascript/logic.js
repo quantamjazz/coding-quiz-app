69fce09dad6c68e5ee5604a480ebde9a1465c6bd
quantamjazz <148623614+quantamjazz@users.noreply.github.com>
2023-12-30 15:21:03 +0000

    added 5 questions to questions.js and event listener to start button on logic.js

---
+++ javascript/logic.js
@@ -0,0 +1,6 @@
+// Assuming your start button has an id 'startButton'
+const startButton = document.querySelector('#startButton');
+
+startButton.addEventListener('click', function() {
+  // Code to start the quiz goes here
+});
--- javascript/questions.js
+++ javascript/questions.js
@@ -12,5 +12,32 @@ On answer click: Check correctness, update timer if wrong, show next question.
 
 // Display high scores, options to restart or clear scores.
 
-//Make responsive and user-friendly.
+//Make responsive and user-friendly.*/
 
+const questions = [
+  {
+    question: "What is the purpose of the `console.log` statement in JavaScript?",
+    answers: ["To write a message to the console", "To display a pop-up message", "To read input from the user", "To clear the console"],
+    correctAnswer: "To write a message to the console"
+  },
+  {
+    question: "Which method is used to round a number to the nearest integer in JavaScript?",
+    answers: ["Math.round()", "Math.floor()", "Math.ceil()", "Math.trunc()"],
+    correctAnswer: "Math.round()"
+  },
+  {
+    question: "How do you declare a constant in JavaScript?",
+    answers: ["var myConst = 5;", "let myConst = 5;", "const myConst = 5;", "myConst = 5;"],
+    correctAnswer: "const myConst = 5;"
+  },
+  {
+    question: "Which statement is used to execute code based on a condition?",
+    answers: ["for", "while", "if", "switch"],
+    correctAnswer: "if"
+  },
+  {
+    question: "What will `console.log(3 + '3')` output in JavaScript?",
+    answers: ["6", "33", "undefined", "Error"],
+    correctAnswer: "33"
+  }
+];
