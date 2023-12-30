52adf3e241c368915f89f438f5ae905d1f1234f5
quantamjazz <148623614+quantamjazz@users.noreply.github.com>
2023-12-30 15:14:29 +0000

    added questions.js and logic.js

---
+++ javascript/questions.js
@@ -0,0 +1,16 @@
+
+/*Initialize quiz variables.
+Show start button.
+
+// On click: Start timer, show first question.
+Question Loop
+
+// Show question and answers.
+On answer click: Check correctness, update timer if wrong, show next question.
+
+// When questions done or timer at 0: Show score, prompt for initials.
+
+// Display high scores, options to restart or clear scores.
+
+//Make responsive and user-friendly.
+
