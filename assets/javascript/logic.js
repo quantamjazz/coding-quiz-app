ca0c66a02e316b59742099f391daa797ffef54eb
quantamjazz <148623614+quantamjazz@users.noreply.github.com>
2023-12-30 15:56:54 +0000

    added styles.css

---
+++ assets/javascript/logic.js
@@ -0,0 +1,31 @@
+import { codingQuestions } from './questions.js';
+
+const startButton = document.getElementById('startButton');
+const quizContainer = document.getElementById('questions');
+let currentQuestionIndex = 0;
+
+function startQuiz() {
+  document.getElementById('start-screen').style.display = 'none';
+  startButton.style.display = 'none';
+  quizContainer.classList.remove('hide');
+  displayQuestion();
+}
+
+function displayQuestion() {
+  if (currentQuestionIndex < codingQuestions.length) {
+    const questionData = codingQuestions[currentQuestionIndex];
+    quizContainer.innerHTML = `
+      <h2>${questionData.question}</h2>
+      <ul>
+        ${questionData.answers.map((answer) => `<li>${answer}</li>`).join('')}
+      </ul>
+    `;
+  } else {
+    // End the quiz when all questions are displayed
+    quizContainer.innerHTML = '<h2>Quiz completed!</h2>';
+  }
+}
+
+// Add event listener to startButton to begin the quiz
+startButton.addEventListener('click', startQuiz);
+
--- javascript/logic.js
+++
@@ -1,6 +0,0 @@
-// Assuming your start button has an id 'startButton'
-const startButton = document.querySelector('#startButton');
-
-startButton.addEventListener('click', function() {
-  // Code to start the quiz goes here
-});
