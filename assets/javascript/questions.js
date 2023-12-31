2722e9421639c2ee5f4aaa721a6b5188fa01dbb5
quantamjazz <148623614+quantamjazz@users.noreply.github.com>
2023-12-31 17:34:16 +0000

    fixed relative path issues and changed variable name in question.js after realising that I used a different one in logic.js for importing

--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -1,4 +1,3 @@
-
 /*Initialize quiz variables.
 Show start button.
 
@@ -14,30 +13,44 @@ On answer click: Check correctness, update timer if wrong, show next question.
 
 //Make responsive and user-friendly.*/
 
-const questions = [
+const codingQuestions = [
   {
-    question: "What is the purpose of the `console.log` statement in JavaScript?",
-    answers: ["To write a message to the console", "To display a pop-up message", "To read input from the user", "To clear the console"],
-    correctAnswer: "To write a message to the console"
+    question:
+      "What is the purpose of the `console.log` statement in JavaScript?",
+    answers: [
+      "To write a message to the console",
+      "To display a pop-up message",
+      "To read input from the user",
+      "To clear the console",
+    ],
+    correctAnswer: "To write a message to the console",
   },
   {
-    question: "Which method is used to round a number to the nearest integer in JavaScript?",
+    question:
+      "Which method is used to round a number to the nearest integer in JavaScript?",
     answers: ["Math.round()", "Math.floor()", "Math.ceil()", "Math.trunc()"],
-    correctAnswer: "Math.round()"
+    correctAnswer: "Math.round()",
   },
   {
     question: "How do you declare a constant in JavaScript?",
-    answers: ["var myConst = 5;", "let myConst = 5;", "const myConst = 5;", "myConst = 5;"],
-    correctAnswer: "const myConst = 5;"
+    answers: [
+      "var myConst = 5;",
+      "let myConst = 5;",
+      "const myConst = 5;",
+      "myConst = 5;",
+    ],
+    correctAnswer: "const myConst = 5;",
   },
   {
     question: "Which statement is used to execute code based on a condition?",
     answers: ["for", "while", "if", "switch"],
-    correctAnswer: "if"
+    correctAnswer: "if",
   },
   {
     question: "What will `console.log(3 + '3')` output in JavaScript?",
     answers: ["6", "33", "undefined", "Error"],
-    correctAnswer: "33"
-  }
+    correctAnswer: "33",
+  },
 ];
+
+export { codingQuestions };
